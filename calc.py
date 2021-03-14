595b3dcfb768d2e5e1b253711438a079e3ecb879
Yulia Mikhaleva <yul10070950@yandex.ru>
2021-03-14 11:16:17 +0300

    Add priorities and all 5 needed data types

--- calc.py
+++ calc.py
@@ -7,8 +7,7 @@ variables = []
 was_digit = False
 str_a = ""
 
-for i in range(len(input_str)):
-	letter = input_str[i]
+for letter in input_str:
 	if letter in "0123456789":
 		was_digit = True
 	if letter in "+-/*^" and was_digit:
@@ -20,19 +19,44 @@ for i in range(len(input_str)):
 		str_a += letter
 
 variables.append(float(str_a))
-result = variables[0]
-for i in range(len(operations)):
-	if result is None or type(result) is str:
+
+priorities = {
+	3:("^",),
+	2:("*", "/"),
+	1:("+", "-")
+}
+
+error, error_val = False, None
+for priority in range(3, 0, -1):
+	if error:
 		break
-	op = operations[i]
-	var = variables[i+1]
-	if op == '/' and var == 0:
-		result = "Inf"
-	elif op == "^":
-		result = result ** var
-	elif op in "+-*/":
-		result = eval(f"{result}{op}{var}")
-	else:
-		result = None
-		
-print("Result: " + str(result))
+	new_variables = [variables[0]]
+	new_operations = []
+
+	for i in range(len(operations)):
+		op = operations[i]
+		var = variables[i+1]
+		if op not in priorities[priority]:
+			new_variables.append(var)
+			new_operations.append(op)
+		else:
+			result = new_variables[-1]
+			if op == '/' and var == 0:
+				error, error_val = True, "Zero division (INF)"
+				break
+			elif op == "^":
+				result = result ** var
+			elif op in "+-*/":
+				result = eval(f"{result}{op}{var}")
+			else:
+				error, error_val = True, "Unsupported operation"
+				break
+			new_variables[-1] = result
+
+	variables = new_variables
+	operations = new_operations
+
+if error:
+	print(f"Error during execution, {error_val=}")
+else:
+	print(f"Result: {variables[0]}")
