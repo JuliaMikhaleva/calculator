50fc467887317a6eb0af5231150bf4bedb3a5401
Yulia Mikhaleva <yul10070950@yandex.ru>
2021-06-04 09:06:32 +0300

    Добавила запись на пробное занятие

--- bot.py
+++ bot.py
@@ -9,8 +9,31 @@ from secret import TOKEN
 
 # Подключаюсь к созданному боту
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
-# Здесь буду хранить выбранный пользователем филиал
+
+
+# Здесь буду хранить информацию о пользователе
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.first_name = None
+        self.last_name = None
+        self.department = None
+        self.training_day = None
+
+    def is_registered(self):
+        # Проверяю, что ребенок уже был записан
+        # Ввод фамилии является последним этапом при записи ребенка
+        return self.last_name is not None
+
+
+# Создаю структуру для поиска информации о пользователе по user_id
 my_clients = dict()
+
+
+# Создаю карточку для новых пользователей
+def user_registration(user_id):
+    if user_id not in my_clients:
+        my_clients[user_id] = User(user_id)
 
 
 # Прописываю стартовое сообщение и возможные опции в виде кнопочек
@@ -20,32 +43,46 @@ def send_welcome(message):
     chat_id = message.chat.id
     # Нахожу пользователя
     user_id = message.from_user.id
+    # Регистрирую пользователя в системе
+    user_registration(user_id)
     # Место для хранения кнопочек по одной в ряд
     # Нашла здесь https://github.com/eternnoir/pyTelegramBotAPI#reply-markup
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
     # Создаю сами кнопочки
-    # Если пользователь уже успел выбрать филиал то
-    if user_id in my_clients:
-        department = my_clients[user_id]
-        itembtns = [
-            types.KeyboardButton("Ваш филиал: " + str(department)),
-            types.KeyboardButton("Узнать расписание"),
-            types.KeyboardButton("Узнать стоимость"),
-            types.KeyboardButton("Как добраться"),
-        ]
-    # Иначе направить на выбор филиала
+    itembtns = list()
+    if my_clients[user_id].is_registered():
+        itembtns.append("Ваш ребенок уже записан!")
     else:
-        itembtns = [
-            types.KeyboardButton("Выбрать филиал")
-        ]
+        itembtns.append("Записать ребенка на пробное занятие")
+    itembtns += [
+        "Получить дополнительную информацию"
+    ]
     # Добавляю их в выделенную выше структуру
     markup.add(*itembtns)
     # Отправляю стартовое сообщение и прикрепляю структуру с кнопочками
     bot.send_message(chat_id, "Здраствуйте, чем я могу Вам помочь?", reply_markup=markup)
 
 
-# Выбор филиала
-@bot.message_handler(regexp="Выбрать филиал")
+@bot.message_handler(regexp="Получить дополнительную информацию")
+def additional_info(message):
+    chat_id = message.chat.id
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+    itembtns = [
+        types.KeyboardButton("Узнать расписание"),
+        types.KeyboardButton("Узнать стоимость"),
+        types.KeyboardButton("Узнать адрес"),
+    ]
+    markup.add(*itembtns)
+    # Добавляю к сообщению палец вниз для мотивации нажатия на кнопочки
+    bot.send_message(chat_id, "Выберите один из вариантов ниже \U0001F447", reply_markup=markup)
+
+
+@bot.message_handler(regexp="Записать ребенка на пробное занятие")
+def registration_training(message):
+    # Запускаю поэтапную запись на занятие
+    send_choose_department(message)
+
+
 def send_choose_department(message):
     chat_id = message.chat.id
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=2)
@@ -55,46 +92,63 @@ def send_choose_department(message):
     ]
     markup.add(*itembtns)
     bot.send_message(chat_id, "Выберите интересующий вас филиал", reply_markup=markup)
+    # Выбираю функцию для обработки следующего сообщения
+    bot.register_next_step_handler(message, training_choose_date)
 
 
-# Привязываю выбранный филиал к пользователю
-@bot.message_handler(regexp="Филиал ")
-def send_set_department(message):
-    # Снова нахожу куда отправлять ответ
+def training_choose_date(message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    department = None
-    if message.text == "Филиал ЦСКА":
-        department = "ЦСКА"
-    if message.text == "Филиал Электрозаводская":
-        department = "Электрозаводская"
-    if department is not None:
-        my_clients[user_id] = department
-        bot.send_message(chat_id, "Выбор зафиксирован")
+    # Записываю филиал в карточку пользователя
+    my_clients[user_id].department = message.text
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=2)
+    if "ЦСКА" in message.text:
+        itembtns = [
+            types.KeyboardButton("Среда"),
+            types.KeyboardButton("Пятница"),
+        ]
     else:
-        bot.send_message(chat_id, "Ошибка")
+        itembtns = [
+            types.KeyboardButton("Вторник"),
+            types.KeyboardButton("Четверг"),
+        ]
+    markup.add(*itembtns)
+    bot.send_message(chat_id, "Выберите день недели", reply_markup=markup)
+    bot.register_next_step_handler(message, enter_personal_info)
+
+
+def enter_personal_info(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    my_clients[user_id].training_day = message.text
+    bot.send_message(chat_id, "Введите имя ребенка")
+    bot.register_next_step_handler(message, enter_surname)
+
+
+def enter_surname(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    my_clients[user_id].first_name = message.text
+    bot.send_message(chat_id, "Введите фамилию ребенка")
+    bot.register_next_step_handler(message, enter_success)
+
+
+def enter_success(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    my_clients[user_id].last_name = message.text
+    bot.send_message(chat_id, "Ребенок записан!")
 
 
 # Обрабатываю "Узнать расписание"
 @bot.message_handler(regexp="Узнать расписание")
 def send_timetable(message):
     chat_id = message.chat.id
-    user_id = message.from_user.id
-    department = my_clients.get(user_id)
-    message = "Ошибка"
-    if department == "ЦСКА":
-        message = "На ЦСКА занятия проходят по средам и пятницам с 19:00 до 20:00"
-    if department == "Электрозаводская":
-        message = "На Электрозаводской занятия проходят по вторникам и четвергам с 18:00 до 19:00"
-    # Отправляю в этот чат ответ
+    message = """
+На ЦСКА занятия проходят по средам и пятницам с 19:00 до 20:00"
+На Электрозаводской занятия проходят по вторникам и четвергам с 18:00 до 19:00
+"""
     bot.send_message(chat_id, message)
-
-
-# Пример из документации (чтобы показать, что бот работает на недоделанных кнопочках)
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    # Отвечаю на сообщение и отвечаю текстом самого запроса
-    bot.reply_to(message, message.text)
 
 
 # Запускаю бота на обработку поступающих запросов
